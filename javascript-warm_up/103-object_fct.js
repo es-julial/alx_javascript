f7edc4a6750765edb0dfaa8f30ddede7afe52beb
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-09-04 12:10:54 +0200

    JavaScript_Warm-Up

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
-
+/*
+Update this script by adding a new
+function incr that increments the integer value.
+*/
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-  this.value++;
+
+myObject.incr = function incr() {
+  myObject.value += 1;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
